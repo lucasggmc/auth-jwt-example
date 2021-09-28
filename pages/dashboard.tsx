cff052fff3b495280e639d672cd80a887de4c38c
lucasggmc <lucasggmc@gmail.com>
2021-09-27 22:52:16 -0300

    salvando e recuperando o token com a lib nookies

---
+++ pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../contexts/AuthContext"
+import { api } from "../services/api";
+
+
+export default function Dashboard(){
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        api.get('/me').then(response => console.log(response));
+    })
+
+    return (
+        <h1>Dash - {user?.email}</h1>
+    )
+}
